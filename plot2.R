d4b66c064b2bbbb8c85d3a0532898cd34f814c3f
Georgia Villapouca <georgiavillapouca@gmail.com>
2017-06-06 23:08:36 -0300

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,25 @@
+# Coursera - Exploratory Data Analysis - Course Project 1
+# Georgia Villapouca - June 2017
+
+# Plot 2
+
+# Preparing data
+data_full = read.csv("./household_power_consumption.txt", header=T, sep=';', 
+				na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, 
+				comment.char="", quote='\"'
+			   )
+data_full$Date = as.Date(data_full$Date, format="%d/%m/%Y")
+
+data = subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+datetime = paste(as.Date(data$Date), data$Time)
+data$Datetime = as.POSIXct(datetime)
+
+# Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+# Saving
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
